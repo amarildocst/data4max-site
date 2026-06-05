9321ccd0a62830bd75fdf25c024a084a70bf1192
amarildocst <amarildocst@hotmail.com>
2026-06-05 12:01:46 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,16 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbzAPhGKtr2tcgy9_w3tW3OyncdMMVoJt9ADlLSVI1AwQG_xz1vTgtLkWGa4uYhV5NotfA/exec"; // URL do Google Apps Script
+const scriptURL = "https://script.google.com/macros/library/d/1j-inKpFY2ZoUvYdfLXQiUZIGTKE8_hh0IRIT5huYqqolga1rLlC_W5tX/1"; // URL do Google Apps Script publicado
 const form = document.getElementById("formInscricao");
 const mensagem = document.getElementById("mensagemConfirmacao");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
-  const data = {
-    nome: document.getElementById("nome").value,
-    email: document.getElementById("email").value,
-    interesse: document.getElementById("interesse").value,
-    consentimento: document.getElementById("consentimento").checked ? "Aceito" : "Não aceito"
-  };
 
-  fetch(scriptURL, {
-    method: "POST",
-    body: JSON.stringify(data)
-  })
-  .then(response => {
-    form.style.display = "none";
-    mensagem.style.display = "block";
-  })
-  .catch(error => alert("Erro ao enviar inscrição: " + error));
+  const data = new FormData(form);
+
+  fetch(scriptURL, { method: "POST", body: data })
+    .then(response => {
+      form.style.display = "none";
+      mensagem.style.display = "block";
+    })
+    .catch(error => alert("Erro ao enviar inscrição: " + error));
 });
